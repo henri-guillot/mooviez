9e919d5bec47c36e34b08604a2640058400e19f6
henri guillot <henri.guillot@hotmail.com>
2023-12-12 13:13:07 +0100

    validations completed

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
   # A bookmark must be linked to a movie and a list, and the [movie, list] pairings should be unique.
-  validates_uniqueness_of :movie, scope: :list
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
   # The comment of a bookmark cannot be shorter than 6 characters.
   validates :comment, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
   # You can’t delete a movie if it is referenced in at least one bookmark.
-
 end
