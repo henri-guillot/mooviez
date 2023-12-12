d0e6db88fa6136b532998c61dfa01a5e4441154c
henri guillot <henri.guillot@hotmail.com>
2023-12-12 21:03:35 +0100

    bookmark controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,4 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
 
   def new
@@ -10,16 +9,17 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to list_path(@list), notice: "Bookmark was successfully created."
     else
-      render :new
+      render 'lists/show'
     end
   end
 
-  def destroy
-    @bookmark.destroy
-    redirect_to list_path(@bookmark.list), status: :see_other
-  end
+  # def destroy
+  # @bookmark = Bookmark.find(params[:id])
+  #   @bookmark.destroy
+  #   redirect_to list_path(@bookmark.list), status: :see_other
+  # end
 
   private
 
@@ -27,9 +27,6 @@ class BookmarksController < ApplicationController
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
 
   def set_list
     @list = List.find(params[:list_id])
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,7 +7,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    # @bookmark = Bookmark.new
+    @bookmark = Bookmark.new
     # @review = Review.new(list: @list)
   end
 
